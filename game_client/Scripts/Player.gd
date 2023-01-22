6b7e220436748dc036e353ff989e987635646441
LucaPalumbo <luca183.palumbo@gmail.com>
2023-01-22 23:59:36 +0100

    test on floor and bullet

--- game_client/Scripts/Player.gd
+++ game_client/Scripts/Player.gd
@@ -1,11 +1,22 @@
 extends KinematicBody2D
 
+const GRAVITY = 4000
+export (int) var speed = 100
+var velocity = Vector2()
+
+
 func _ready():
 	pass # Replace with function body.
 
-export (int) var speed = 200
+func _physics_process(delta):
+	get_input()
+	velocity.y += delta * GRAVITY
+	velocity = move_and_slide(velocity)
 
-var velocity = Vector2()
+
+func _input(event):
+	if event.is_action_pressed("mouse_click"):
+		print("shoot")
 
 func get_input():
 	velocity = Vector2()
@@ -13,15 +24,8 @@ func get_input():
 		velocity.x += 1
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-	#if Input.is_action_pressed("down"):
-	#	velocity.y += 1
-	#if Input.is_action_pressed("up"):
-	#	velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- game_client/Scripts/Terrain.gd
+++ game_client/Scripts/Terrain.gd
@@ -1,46 +1,16 @@
-extends CollisionPolygon2D
+extends Node2D
 
 
-var collision_holder : Node2D
-var world_size : Vector2
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_to_group("destructibles")
-	world_size = (get_parent() as Sprite2D).get_rect().size
+	pass # Replace with function body.
 
 
-
-func build_collisions_from_image():	
-	# Create bitmap from the SubViewport (which projects into our sprite)
-	var bitmap := BitMap.new()
-	bitmap.create_from_image_alpha($Sprite2D.texture.get_image())
-	
-	# DEBUG:
-	#$Sprite2D.get_texture().get_data().save_png("res://screenshots/debug" + get_parent().name + ".png")
-	#print("Saved")
-
-	# This will generate polygons for the given coordinate rectangle within the bitmap
-	# In our case, our given coordinates are the entire image.
-	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0,0), bitmap.get_size()))
-
-	# Now create a collision polygon for each polygon returned
-	# For the most part there will probably only be one.... unless you have islands
-	for polygon in polygons:
-		var collider := CollisionPolygon2D.new()
-
-		# Remap our points from the viewport coordinates back to world coordinates.
-		var newpoints := Array()
-		for point in polygon:
-			newpoints.push_back(_viewport_to_world(point))
-		collider.polygon = newpoints
-		collision_holder.add_child(collider)
-
-
-func _viewport_to_world(point : Vector2) -> Vector2:
-	var dynamic_texture_size = $SubViewport.get_size()
-	return Vector2(
-		((point.x + get_viewport_rect().position.x) / dynamic_texture_size.x) * world_size.x,
-		((point.y + get_viewport_rect().position.y) / dynamic_texture_size.y) * world_size.y
-	)
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
